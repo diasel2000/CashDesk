6336b3a15f0e85150433e2aaed75128be597937d
diasel2000 <solobot62@gmail.com>
2019-07-12 17:12:07 +0300

    first commit

---
+++ src/main/java/cashdesk/controller/HelloServlet.java
@@ -0,0 +1,17 @@
+package cashdesk.controller;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+
+    }
+}
